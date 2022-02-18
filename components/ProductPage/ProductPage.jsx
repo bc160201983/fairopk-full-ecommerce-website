94c2cc1cc442a80f8249cdcc438f963952316419
bilal afzaal <softnacuk@gmail.com>
2022-02-18 22:04:29 +0500

    update

--- components/ProductPage/ProductPage.jsx
+++ components/ProductPage/ProductPage.jsx
@@ -1,8 +1,9 @@
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 const ProductPage = ({ product }) => {
   const [productData, setProductData] = useState(product || []);
-
+  console.log(productData);
   return (
     <div className="max-w-screen-xl mx-auto">
       <div className="first-row grid lg:grid-cols-2 grid-cols-1 h-[400px]">
@@ -12,13 +13,22 @@ const ProductPage = ({ product }) => {
         <div className="product-info pt-5 flex-1 flex flex-col justify-evenly mt-5">
           <div className="info px-4">
             <div className="title-btn my-2 flex justify-between">
-              <div className="name pr-[2px]">CovidSelf COVID Self Text Kit</div>
+              <div className="name pr-[2px] font-bold text-[14px]">
+                {productData[0].name}
+              </div>
               <div className="btn text-[14px] text-[#0c831f] rounded text-center w-[100px] h-7 border-2 border-[#CCCCCC]">
                 add
               </div>
             </div>
-            <div className="unit mb-2">1 unit</div>
-            <div className="price font-bold text-[14px]">Rs224</div>
+            <div className="unit mb-2 text-sm">
+              Category :
+              <Link href={"#"}>
+                <a>{productData[0].categories[0].name}</a>
+              </Link>
+            </div>
+            <div className="price font-bold text-[14px]">
+              Rs{Math.trunc(productData[0].price)}
+            </div>
           </div>
           <div className="unti-section items-center">
             <div className="flex flex-col  m-3 justify-start">
